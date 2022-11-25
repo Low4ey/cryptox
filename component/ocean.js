0e8e5d5aee9e1e459954a8122e044d30f90d3d9c
Ajeet Kumar <ajeet8364@gmail.com>
2022-11-25 19:59:28 +0530

    1.3

--- component/ocean.js
+++ component/ocean.js
@@ -4,13 +4,12 @@ import style from "../styles/ocean.module.css"
 const Ocean = () => {
   return (<>
   <div>
-    <div className={style.bg}></div>
+    <div className={style.bg}>
+      <h1 className={style.hero_tag_head}>HAVELOCK ISLAND</h1>
+      <p className={style.hero_tag_parg}>Travel Guide-Top Things To Do & See, Hotels & Resorts, Restaurant & Cafe and More </p>
+    </div>
+    </div>
     <div className={style.black}>
-    </div>
-    <div className={style.text}>
-    <h1>HAVELOCK ISLAND</h1>
-        <p>Travel Guide-Top Things To Do & See, Hotels & Resorts, Restaurant & Cafe and More </p>
-    </div>
     </div>
   </>
   )
